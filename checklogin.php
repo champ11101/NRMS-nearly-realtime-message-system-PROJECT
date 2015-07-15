539367ba0d425f3297f93ee1bf428c9cc4007e83
champ11101 <wchalit@hotmail.com>
2015-07-15 16:41:07 +0700

    NRMS
    index.php = หน้าแสดงข้อความ, ajax

---
+++ checklogin.php
@@ -0,0 +1,49 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no">
+	<link href="bootstrap/css/bootstrap.min.css" rel="stylesheet">
+	<link rel="shortcut  icon" href="NRMS.png">
+</head>
+<body style="background-color:azure">
+
+<?php
+	session_start();
+
+	if($_POST['username'] =="" || $_POST['password'] ==""){
+		echo "<br><div align='center'>Please enter username and password !!</div>";
+?>
+	<br><br>
+
+	<div align="center">
+		<input type="button" class="btn btn-success" name="go_sign_in" id="go_sign_in" value="Back" onclick="window.location='login.html' " />
+	</div>
+<?php
+	}
+	else{
+			mysql_connect("localhost","root","");
+			mysql_select_db("nrms");
+			$strSQL = "SELECT * FROM user_account WHERE username = '".mysql_real_escape_string($_POST['username'])."' and password = '".mysql_real_escape_string($_POST['password'])."'";
+			$objQuery = mysql_query($strSQL);
+			$objResult = mysql_fetch_array($objQuery);
+
+			if(!$objResult){
+				echo "<br><div align='center'>Username or password incorrect !!!</div>";
+?>			<br><br>
+			<div align="center">
+				<input type="button" class="btn btn-success" name="go_sign_in" id="go_sign_in" value="Back" onclick="window.location='login.html' " />
+			</div>
+<?php
+			}
+			else{
+				$_SESSION["ID"] = $objResult["ID"];
+				$_SESSION["name"] = $objResult["name"];
+		
+				session_write_close();
+				mysql_close();
+				header("location:index.php");
+			}
+	}
+?>
+</body>
+</html>
---
+++ getNewMsg.php
@@ -0,0 +1,24 @@
+<?php
+	session_start();
+	$x = $_SESSION['name'];
+	$objConnect = mysql_connect("localhost","root","") or die(mysql_error());
+	mysql_query("SET NAMES UTF8");
+	$objDB = mysql_select_db("nrms");
+	$strSQL = "SELECT * FROM message WHERE (send_to ='$x' AND read_status ='unread') ORDER BY send_time DESC";
+	$objQuery = mysql_query($strSQL) or die (mysql_error());
+	$intNumField = mysql_num_fields($objQuery);
+	$resultArray = array();
+	while($obResult = mysql_fetch_array($objQuery))
+	{
+		$arrCol = array();
+		for($i=0;$i<$intNumField;$i++)
+		{
+			$arrCol[mysql_field_name($objQuery,$i)] = $obResult[$i];
+		}
+		array_push($resultArray,$arrCol);
+	}
+	session_write_close();
+	mysql_close($objConnect);
+	
+	echo json_encode($resultArray);
+?>
